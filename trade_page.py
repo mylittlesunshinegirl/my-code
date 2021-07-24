15415654e62755d886d817af9a60e40e9941b0c9
mylittlesunshinegirl <87892589+mylittlesunshinegirl@users.noreply.github.com>
2021-07-24 16:38:56 +0800

    Update trade_page.py

--- trade_page.py
+++ trade_page.py
@@ -12,23 +12,17 @@ class TradeProcessForCRO(Basepage):
         self.get_element(loc.CROUSDC,"focus on the CROUSDC")
         self.click_element(loc.trade_button,"click the CRO/USDC trade button")
         self.switch_to_iframe(loc.buy_CRO_iframe,"switch to  buy_CRO iframe")
-
         self.input_text(loc.price,"0.1469","input the price")
         self.input_text(loc.amount,"10","input the amount")
         self.click_element(loc.submit,"click the submit button")
-        self.get_element_exists(loc.check_if_scuess,"check wether the element exists or not")
-
-
 
     def sell_CRO(self):
         self.click_element(loc.CRO_markets,"click CRO MARKETS")
-
         self.get_element(loc.CROUSDC,"focus on the CROUSDC")
         self.click_element(loc.trade_button,"click the  CRO/USDC trade button")
         self.switch_to_iframe(loc.sell_CRO_iframe,"switch to  sell_CRO iframe")
         self.input_text(loc.price,"0.1469","input the price")
         self.input_text(loc.amount,"10","input the amount")
         self.click_element(loc.submit,"click the submit button")
-        self.get_element_exists(loc.check_if_scuess,"check wether the element exists or not")
 
 
